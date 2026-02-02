f88a67ce8006e35578d8b5b5a3b0920ec1e3cd43
MizzyMi <173277815+mizzymi@users.noreply.github.com>
2026-02-02 22:39:33 +0100

    fix: :bug: build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'apps', renderMode: RenderMode.Prerender },
+  { path: 'games', renderMode: RenderMode.Prerender },
+  { path: 'studio', renderMode: RenderMode.Prerender },
+  { path: 'contact', renderMode: RenderMode.Prerender },
+
+  { path: 'apps/:id', renderMode: RenderMode.Server },
+  { path: 'games/:id', renderMode: RenderMode.Server },
+
+  { path: '**', renderMode: RenderMode.Prerender },
 ];
