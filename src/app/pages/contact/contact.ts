67f33e6f77d906177f4caa2f4007f0208cc5be06
MizzyMi <173277815+mizzymi@users.noreply.github.com>
2026-02-04 11:55:32 +0100

    feat:

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { TranslocoPipe } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, TranslocoPipe],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
-
+  readonly toEmail = 'shop@reimii.com';
 }
